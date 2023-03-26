32055e1a7a62ef4a2916408199ca19c29030931c
Ronny Ahero <38691932+ron-don@users.noreply.github.com>
2023-03-26 09:10:32 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Microservices-parent
+ A spring boot microservices project that combines a product service, order service, inventory service and notification service
---
+++ order-service/src/main/java/com/ronnytech/orderservice/controller/OrderController.java
@@ -0,0 +1,17 @@
+package com.ronnytech.orderservice.controller;
+
+import com.ronnytech.orderservice.dto.OrderRequest;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/order")
+public class OrderController {
+
+    @PostMapping
+    public String placeOrder(@RequestBody OrderRequest orderRequest) {
+        return "Order Placed Successfully";
+    }
+}
